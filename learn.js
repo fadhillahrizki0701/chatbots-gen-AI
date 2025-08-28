6d901ea646686dc28b9be4ccb6f5959d3c5969d1
fadhillahrizki0701 <fadhillahrizki0701@gmail.com>
2025-08-28 22:38:35 +0700

    first commit

---
+++ learn.js
@@ -0,0 +1,20 @@
+import { GoogleGenAI } from "@google/genai"; //import class utamanya
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const GOOGLE_AI_STUDIO_API_KEY = process.env.GOOGLE_AI_STUDIO_API_KEY;
+// The client gets the API key from the environment variable `GEMINI_API_KEY`.
+const ai = new GoogleGenAI({
+    apiKey: GOOGLE_AI_STUDIO_API_KEY
+});
+
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: "Halo, apakah kamu bisa menjawab pertanyaan ini?",
+  });
+  console.log(response.text);
+}
+
+main();
